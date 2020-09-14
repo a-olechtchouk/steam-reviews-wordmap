05085de4f75b62308ac4f44dc9e337b016311f9e
Andrew Olechtchouk <andrew.olechtchouk@gmail.com>
2020-09-14 09:46:04 -0400

    code cleanup, and working on creating the mask for the Squad image

--- funcmodule.py
+++ funcmodule.py
@@ -51,37 +51,26 @@ stopwords += ['within', 'without', 'would', 'yet', 'you', 'your']
 stopwords += ['yours', 'yourself', 'yourselves']
 
 import matplotlib.pyplot as plt
+import numpy as np
 import requests, re, string, json, os.path
-from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
 
 def get_string_from_params(payload, appid):
-
-    # filter can be r (recent) u (updated) or a (all). 
-    # language is a minimum of 3 characters to store 'all'
-    p_filt_lan  = payload.get('filter')[0] + payload.get('language')[0:3]
 
     p_curs = payload.get('cursor')   
     if p_curs == '*':
         p_curs = 'a'
+    else:
+        p_curs = re.sub('[/]', '', p_curs)
 
-    # day_range and cursor (unless the cursor is asterisk "*") are directly from the payload
-    p_rnge_curs = payload.get('day_range') + p_curs
-
-    # review_type can be a (all) p (positive) or n (negative). 
-    # purchase_type can be a (all) n (non_steam_purchase) or s (steam)
-    # num_per_page is directly from the payload
-    p_types_num = payload.get('review_type')[0] +  payload.get('purchase_type')[0] +  payload.get('num_per_page') 
-
-
-    return p_filt_lan + p_rnge_curs + p_types_num + appid
+    return p_curs
 
 # use a local copy of the requested query if it exists.
 # otherwise, form the query, send it, and write the json response to a file.
-def get_query_data(url, appid, payload, cursor="*"):
-    payload['cursor'] = cursor
+def get_query_data(url, appid, payload):
 
     filename = get_string_from_params(payload, appid)
-
 
     if os.path.isfile(filename) is True:    # the file exists, so use the local query data instead.
         print("the local query " + filename + " exists!")
@@ -91,46 +80,53 @@ def get_query_data(url, appid, payload, cursor="*"):
         
     else:                                   # otherwise, query Steam and write that data locally.
         print("the local query " + filename + " does NOT exist!")
+
         full_url = url + appid + '?json=1?'
 
         # send the request to Steam, check its response, and write the data to a file.
-        r = requests.get(full_url, params=payload) 
+        r = requests.get(full_url, params=payload)
+
+        if r.text == '{"response":{}}':
+            return -1
 
         check_assertions(r.status_code, 'init_stat_code')
 
         json_form = r.json()
 
         check_assertions(json_form.get('success'), 'query_success')
+
         
         f = open(filename, 'w')
         json.dump(json_form, f)
         f.close()
 
-    print_query_response_info(json_form)
     return json_form
 
-# get and print basic info for debugging
-def print_query_response_info(json_form):
-
-    cursor_ret = json_form.get('cursor')
-    summary_ret = json_form.get('query_summary')
-
-    print(cursor_ret)
-    print(summary_ret) 
-
 def createWordCloud(text):
-    # Generate a word cloud image
-    wordcloud = WordCloud().generate(text)
 
-    # Display the generated image the matplotlib way:
-    plt.imshow(wordcloud, interpolation="bilinear")
+    squadmask = np.array(Image.open("squadlogo_black_gold_hires.png"))
+    
+    # Generate a word cloud image
+    wc = WordCloud(mask=squadmask, contour_width=3, contour_color='steelblue')
+    
+    wc.generate(text)
+
+    # image_colors = ImageColorGenerator(mask)
+    image_colors = ImageColorGenerator(squadmask)
+
+    # store to file
+    # wc.to_file("wordmap.png")
+
+    # show
+    plt.imshow(wc.recolor(color_func=image_colors), interpolation='bilinear')
+    # plt.axis("off")
+    # plt.figure()
+    plt.imshow(squadmask, cmap=plt.cm.get_cmap('gray'), interpolation='bilinear')
     plt.axis("off")
-    plt.savefig('wordmap.svg', dpi=600)
     plt.show()
 
 def removeWords(wordlist, removedwords):
     return [w for w in wordlist if w not in removedwords]
-
 
 def check_assertions(assertion_value, assertion_type):
 
@@ -141,13 +137,3 @@ def check_assertions(assertion_value, assertion_type):
 
         elif assertion_type == 'query_success': 
             if not assertion_value == 1: raise AssertionError
-
-def print_simple_review(review):
-
-    author_stats = review.get('author')
-
-    total_hours = author_stats.get('playtime_forever')
-    rev_text = review.get('review')
-
-    print('Total hours played: ' + str(total_hours))
-    print('Full review: ' + str(rev_text))
--- steam_reviews_wordmap.py
+++ steam_reviews_wordmap.py
@@ -1,19 +1,29 @@
 import re, string
 from funcmodule import *
+from datetime import datetime
 
-steamwords = ['game', 'good', 'great', 'lot', 'youre', 'your', 'fun', 'play', 'dont', 'ive'] # the list of steam words are being updated as time goes on
+steamwords = ['game', 'good', 'great', 'lot', 'youre', 'your', 'fun', 'play', 'dont', 'ive', 'really', 'thing', 'games'] # the list of steam words are being updated as time goes on
 removedwords = stopwords + steamwords    
 
 def process_multiple_queries(url, appid, payload, iters):
 
     total_processed = 0
     key_words = []
+    prev_cursor = ''
 
     for x in range(iters):
 
-        prev_cursor = query.get('cursor')
-
         query = get_query_data(url, appid, payload)         # get query data, either locally or from Steam.\
+        # print(query)
+        print(('query_summary is : ') + str(query.get('query_summary')))
+        payload['cursor'] = query.get('cursor')
+        # print(query.get('cursor'))
+
+        if query.get("query_summary").get("num_reviews") == 0:
+            break
+
+        if query == -1:
+            break
 
         if prev_cursor == query.get('cursor'):
             break
@@ -23,12 +33,11 @@ def process_multiple_queries(url, appid, payload, iters):
 
         num_reviews = query.get('query_summary').get('num_reviews')
         total_processed = total_processed + num_reviews
-        payload['cursor'] = query.get('cursor')
-
-        
+        prev_cursor = query.get('cursor')
+   
     # IMPORTANT! MAKE SURE TO STOP GETTING REQ'S FOR THIS URL
-    full_url = url + appid + '?json=1?'
-    requests.get(full_url, timeout=1)
+    # full_url = url + appid + '?json=1?'
+    # requests.get(full_url, timeout=0.001)
 
     print('processed a total of: ' + str(total_processed) + ' reviews.')
     return key_words
@@ -38,6 +47,8 @@ def filter_key_words(reviews):
     key_words = []
     for x in reviews:
         rev_text = x.get('review').lower()                                          # set the text to lowercase,
+        dt_object = datetime.fromtimestamp(x.get('timestamp_created'))
+        print("dt_object =", dt_object)
         rev_text = rev_text.translate(str.maketrans('', '', string.punctuation))    # remove punctuation, 
         rev_text = re.findall(r"(?i)\b[a-z]+\b", rev_text)                          # keep english alphabet characters,
         rev_text = list(dict.fromkeys(rev_text))                                    # remove duplicate words,
@@ -48,13 +59,17 @@ def filter_key_words(reviews):
 # set-up the initial url and payload
 appid = str(393380)
 url = 'http://store.steampowered.com/appreviews/'
-payload = {'filter': 'recent', 'language': 'english', 'day_range': '1', 'review_type': 'all', 'purchase_type': 'all', 'num_per_page': '100'}
+payload = {'filter': "recent", 'language': "english", 'cursor': "*", 'day_range': "3650", 'review_type': "all", 'purchase_type': "all", 'num_per_page': "100"}
 
-num_total_requests = int(34000 / 100)
-
+num_total_requests = int(40000 / 100)
 
 key_words = process_multiple_queries(url, appid, payload, num_total_requests)
 
 text = ' '.join(key_words)                          # finally convert the list of filtered words into one long string
+
+# save the final string of words to a textfile
+f2 = open('output.txt', 'w')
+f2.write(text)
+f2.close()
 
 createWordCloud(text)                               # create and display a wordcloud
