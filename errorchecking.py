d76cd495b716bc2ce0a2aed564d596ab1e6b0ef2
Andrew Olechtchouk <andrew.olechtchouk@gmail.com>
2020-09-14 22:33:26 -0400

    error checking for getting the query data - the code - should look cleaner. yet to test

--- errorchecking.py
+++ errorchecking.py
@@ -1,21 +1,16 @@
-def check_assertions(assertion_value, assertion_type):
-    if __debug__:
+import requests
+from requests import Response
+import simplejson as json
+
+def is_query_response_valid(r: Response):
+    if r.status_code != 200: return False            # ensure response status code is 200 
+    if r.text == '{"response":{}}': return False     # ensure the (JSON-encoded) response list isnt empty
+
+
+    json_dict = r.json()                                    # at this point we can try to decode the JSON into a dict
+    if json_dict['success'] != 1: return False              # ensure the query was successful
     
-        if assertion_type == 'init_stat_code':
-            if not assertion_value == 200: raise AssertionError
+    query_summary = json_dict['query_summary']   
+    if query_summary['num_reviews'] <= 0: return False      # ensure we got some reviews in the response (more than 0)
 
-        elif assertion_type == 'query_success': 
-            if not assertion_value == 1: raise AssertionError
-
-
-def check_empty_query_response(query, prev_c):
-    if query.get("query_summary").get("num_reviews") == 0:
-        return True
-
-    if query == -1:
-        return True
-
-    if prev_c == query.get('cursor'):
-        return True
-    
-    return False
+    return json_dict                                        # at this point we should be okay to return json_dict
