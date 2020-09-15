22a8417acce028d08fc066d5ea49d10e72dd08fc
Andrew Olechtchouk <andrew.olechtchouk@gmail.com>
2020-09-14 23:03:32 -0400

    removed all the functions out of funcmodule.py, and general code cleanup. not tested

--- inout.py
+++ inout.py
@@ -1,15 +1,15 @@
 from json import load, dump
 
 # read and write JSON-formatted files
-def rw_json(name, mode, json_form=None):
+def rw_json(name, mode, json_dict=None):
 
     f = open(name, mode)
 
-    if mode == 'r': json_form = load(f)
-    if mode == 'w': dump(json_form, f)
+    if mode == 'r': json_dict = load(f)
+    if mode == 'w': dump(json_dict, f)
 
     f.close()
-    return json_form
+    return json_dict
 
 
 #  read and write text files
@@ -22,32 +22,3 @@ def rw_txt(name, mode, text=None):
     
     f.close()
     return text
-
-
-# Create the wordcloud and save the image/s
-def produce_wordcloud(mask_img, text, name):
-    import matplotlib.pyplot as plt
-    import numpy as np
-    from wordcloud import WordCloud, ImageColorGenerator
-    from PIL import Image
-
-    # Init the wordcloud mask from an image
-    mask = np.array(Image.open(mask_img + ".png"))
-
-    # Generate a word cloud from text, and bound it inside the mask contour outline
-    wc = WordCloud(scale=1, stopwords=None, width=2560, height=1440,mask=mask, contour_width=4, contour_color='white')
-    wc.generate(text)
-
-    # Init the color function, so the wordcloud can use colors from the mask
-    image_colors = ImageColorGenerator(mask)
-
-    plt.figure(dpi=300)
-    plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
-    plt.axis("off")
-    # plt.show()
-    
-    # Save the image/s in various file formats (png, tiff, and jpg)
-    plt.savefig(name + '.png')
-    wc.to_file(name + '.png')
-    wc.to_file(name + '.tiff')
-    wc.to_file(name + '.jpg')
--- steam_reviews_wordmap.py
+++ steam_reviews_wordmap.py
@@ -1,7 +1,42 @@
-import re, string
-from funcmodule import *
-import funcmodule as 
-from datetime import datetime
+import requests, os.path, re
+import errorchecking as err
+import inout
+
+def get_string_from_params(payload, appid):
+
+    p_curs = payload.get('cursor')   
+    if p_curs == '*':
+        p_curs = 'a'
+    else:
+        p_curs = re.sub('[/]', '', p_curs)
+
+    return p_curs
+
+# use a local copy of the requested query if it exists.
+# otherwise, form the query, send it, and write the json response to a file.
+def get_query_data(url, appid, payload):
+
+    filename = get_string_from_params(payload, appid)
+
+    if os.path.isfile(filename):                    # the file exists, so load local query data
+        print("the local query " + filename + " exists!")
+        json_dict = inout.rw_json(filename, 'r') 
+ 
+    else:                                           # its not cached locally, so query Steam
+        print("the local query " + filename + " does NOT exist!")
+
+        full_url = url + appid + '?json=1?'         # form the complete URL,
+        r = requests.get(full_url, params=payload)  # send a GET request,
+
+        # validate the response we got
+        json_dict = err.is_query_response_valid(r)
+        if json_dict is False: return False
+
+        # it was valid, so write the query to a file locally
+        inout.rw_json(filename, 'w', json_dict=json_dict)   
+    
+    return json_dict
+
 
 def process_multiple_queries(url, appid, payload, iters):
 
@@ -11,14 +46,16 @@ def process_multiple_queries(url, appid, payload, iters):
 
     for x in range(iters):
 
-        query = get_query_data(url, appid, payload)         # get query data, either locally or from Steam.\
+        # get query GET response (json_dict) data, either locally or from Steam, and get its validity
+        query = get_query_data(url, appid, payload)
 
-        print(('query_summary is : ') + str(query.get('query_summary')))
-
-        payload['cursor'] = query.get('cursor')
-
-        if check_empty_query_response(query, prev_cursor):
+        # ensure that its valid, and that it differs from the previous query response.
+        # if not, break the loop and exit 
+        if (query is False) or (prev_cursor == query['cursor']):
             break
+        
+        # the response is new and valid, so update the 'cursor' for the next payload (next GET request)
+        payload['cursor'] = query['cursor']
 
         review_list = list(query.get('reviews'))            # specify only review information
         key_words += filter_key_words(review_list)          # get a list of 'key' words by filtering out the words from each review text
@@ -26,17 +63,13 @@ def process_multiple_queries(url, appid, payload, iters):
         num_reviews = query.get('query_summary').get('num_reviews')
         total_processed = total_processed + num_reviews
         prev_cursor = query.get('cursor')
+
+        print(('query_summary is : ') + str(query.get('query_summary')))
    
-    # IMPORTANT! MAKE SURE TO STOP GETTING REQ'S FOR THIS URL
-    # full_url = url + appid + '?json=1?'
-    # requests.get(full_url, timeout=0.001)
+
 
     print('processed a total of: ' + str(total_processed) + ' reviews.')
     return key_words
-
-
-
-
 
 
 # set-up the initial url and payload
@@ -56,3 +89,62 @@ f2.write(text)
 f2.close()
 
 createWordCloud(text)                               # create and display a wordcloud
+
+
+# Create the wordcloud and save the image/s
+def produce_wordcloud(mask_img, text, name):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from wordcloud import WordCloud, ImageColorGenerator
+    from PIL import Image
+
+    # Init the wordcloud mask from an image
+    mask = np.array(Image.open(mask_img + ".png"))
+
+    # Generate a word cloud from text, and bound it inside the mask contour outline
+    wc = WordCloud(scale=1, stopwords=None, width=2560, height=1440,mask=mask, contour_width=4, contour_color='white')
+    wc.generate(text)
+
+    # Init the color function, so the wordcloud can use colors from the mask
+    image_colors = ImageColorGenerator(mask)
+
+    plt.figure(dpi=300)
+    plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
+    plt.axis("off")
+    # plt.show()
+    
+    # Save the image/s in various file formats (png, tiff, and jpg)
+    plt.savefig(name + '.png')
+    wc.to_file(name + '.png')
+    wc.to_file(name + '.tiff')
+    wc.to_file(name + '.jpg')
+
+
+
+
+
+
+
+
+
+            # f = open(filename, 'r')
+        # json_form = json.load(f)
+        # f.close()
+
+
+                # f = open(filename, 'w')
+        # json.dump(json_form, f)
+        # f.close()
+
+
+        # import matplotlib.pyplot as plt
+# import numpy as np
+# import requests, re, string, json, os.path
+# from wordcloud import WordCloud, ImageColorGenerator
+# from PIL import Image
+
+
+
+    # IMPORTANT! MAKE SURE TO STOP GETTING REQ'S FOR THIS URL
+    # full_url = url + appid + '?json=1?'
+    # requests.get(full_url, timeout=0.001)
