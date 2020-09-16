36c91f4d585d713ec7636e4631ef49471fa904c9
Andrew Olechtchouk <andrew.olechtchouk@gmail.com>
2020-09-16 15:25:37 -0400

    added file trie.py which is a tree datastructure used to store gamename and appid pairings. will be used for the database. added supporting functions in appiddb, but i have not implemented the trie part yet

--- appiddb.py
+++ appiddb.py
@@ -6,15 +6,64 @@ from requests import Response
 import simplejson as json
 
 import time
+import trie
 
-def process_gameslist(json_dict: dict):
+from operator import itemgetter, attrgetter
 
-    applist = json_dict['applist']
-    print(applist)
+def check_valid_user_input(appid, name):
+    assert((appid != None) or (name != None)), 'The appid and name was empty!'
 
+def get_tuples_from_games(gamesdict: dict):
+    
+    gamelist = list(gamesdict.get('applist').get('apps'))
+
+    tuple_list = []
+
+    for game in gamelist:
+        title = game['name']
+        appid = int(game['appid'])
+
+        cur_tuple = (title, appid)
+        tuple_list.append(cur_tuple)
+
+    print('There are' , len(tuple_list), 'applications/games in the database.')
+    sorted_tuple_list = sorted(tuple_list, key=itemgetter(0))
+
+    return sorted_tuple_list
+
+def build_trie_from_games(gamesdict: dict):
+
+    gamelist = list(gamesdict.get('applist').get('apps'))
+    print(gamelist[5])
+
+    sorted_gamelist = sorted(gamelist, key=attrgetter('name'))
+    print(sorted_gamelist[5])
+    
+
+    # parent trie
+    # root_trie = trie.Node()
+    # for game in gamelist:
+
+    #     trie.insert
+
+    # return root_trie
+
+
+def get_id_and_name(appid=None, name=None):
+
+    # if (appid==None) and (name==None):
+    try:
+        check_valid_user_input(appid, name)
+    except AssertionError as error:
+        print(error)
+        print('Please enter a valid appid OR name!')
+        check_valid_user_input(appid, name)
+
+# get the dictionary of all {appid, name} pairings for apps from Steam.
+# if the local query response exists load it, otherwise send 1 request to Steam and store it.
 def get_gameslist(url, filename):
 
-    filename += 'steamgamelist'
+    filename += 'steamgamelistraw'
 
     if os.path.isfile(filename):                    # the file exists, so load local query data
         print("the local query " + filename + " exists!")
@@ -46,4 +95,19 @@ urldb = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/'
 cwpath = os.getcwd() + '/'
 filename = cwpath + 'appiddump/'
 json_dict = get_gameslist(urldb, filename)
-process_gameslist(json_dict)
+
+sorted_games_tuple_list = get_tuples_from_games(json_dict)
+# build_trie_from_games(json_dict)
+
+# get_id_and_name(appid=None, name='g')
+
+
+
+
+        # if appid: return game[appid]
+        # if name: return game[name]
+
+        # if game['name'] == 'Squad': print(game)
+
+
+    # next(item for item in dicts if item["name"] == "Pam")
---
+++ trie.py
@@ -0,0 +1,88 @@
+# NOTE: NONE OF THE FOLLOWING CODE BELOW IS MINE. I DID NOT WRITE 1 LINE OF IT.
+# IT IS TAKEN FROM THE WIKI PAGE: https://en.wikipedia.org/wiki/Trie#A_Python_version
+
+from typing import Optional, Dict, Any, List
+
+class Node:
+    def __init__(self) -> None:
+        # Note that using a dictionary for children (as in this implementation)
+        # would not by default lexicographically sort the children, which is
+        # required by the lexicographic sorting in the Sorting section.
+        # For lexicographic sorting, we can instead use an array of Nodes.
+        self.children: Dict[str, Node] = {}  # mapping from character to Node
+        self.value: Optional[Any] = None
+
+def find(node: Node, key: str) -> Optional[Any]:
+    # Find value by key in node.
+    for char in key:
+        if char in node.children:
+            node = node.children[char]
+        else:
+            return None
+    return node.value
+
+def insert(node: Node, key: str, value: Any) -> None:
+# """Insert key/value pair into node."""
+    for char in key:
+        if char not in node.children:
+            node.children[char] = Node()
+        node = node.children[char]
+    node.value = value
+
+def delete(root: Node, key: str) -> bool:
+    # """
+    # Eagerly delete the key from the trie rooted at `root`.
+    # Return whether the trie rooted at `root` is now empty.
+    # """
+    
+    def _delete(node: Node, key: str, d: int) -> bool:
+        # """
+        # Clear the node corresponding to key[d], and delete the child key[d+1]
+        # if that subtrie is completely empty, and return whether `node` has been
+        # cleared.
+        # """
+        if d == len(key):
+            node.value = None
+        else:
+            c = key[d]
+            if c in node.children and _delete(node.children[c], key, d+1):
+                del node.children[c]
+        # Return whether the subtrie rooted at `node` is now completely empty
+        return node.value is None and len(node.children) == 0
+
+    return _delete(root, key, 0)
+
+def keys_with_prefix(root: Node, prefix: str) -> List[str]:
+    results: List[str] = []
+    x = _get_node(root, prefix)
+    _collect(x, list(prefix), results)
+    return results
+
+def _collect(x: Optional[Node], prefix: List[str], results: List[str]) -> None:
+    # """
+    # Append keys under node `x` matching the given prefix to `results`.
+    # prefix: list of characters
+    # """
+    if x is None:
+        return
+    if x.value is not None:
+        prefix_str = ''.join(prefix)
+        results.append(prefix_str)
+    for c in x.children:
+        prefix.append(c)
+        _collect(x.children[c], prefix, results)
+        del prefix[-1]  # delete last character
+    
+def _get_node(node: Node, key: str) -> Optional[Node]:
+    # """
+    # Find node by key. This is the same as the `find` function defined above,
+    # but returning the found node itself rather than the found node's value.
+    # """
+    for char in key:
+        if char in node.children:
+            node = node.children[char]
+        else:
+            return None
+    return node
+
+    
