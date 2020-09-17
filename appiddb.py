3ee0aa84ebc543575b207f5feda6df657e847c03
Andrew Olechtchouk <andrew.olechtchouk@gmail.com>
2020-09-17 00:19:10 -0400

    updated gui

--- appiddb.py
+++ appiddb.py
@@ -5,16 +5,16 @@ from handlerequests import get_gameslist
 from sys import setrecursionlimit
 import pickle, trie
 
-def check_valid_user_input(appid=None, name):
-    assert((appid != None) or (name != None)), 'The appid and name was empty!'
+# def check_valid_user_input(appid=None, name):
+#     assert((appid != None) or (name != None)), 'The appid and name was empty!'
     
-def get_id_and_name(appid=None, name=None):
-    try:
-        check_valid_user_input(appid, name)
-    except AssertionError as error:
-        print(error)
-        print('Please enter a valid appid OR name!')
-        check_valid_user_input(appid, name)    
+# def get_id_and_name(appid=None, name=None):
+#     try:
+#         check_valid_user_input(appid, name)
+#     except AssertionError as error:
+#         print(error)
+#         print('Please enter a valid appid OR name!')
+#         check_valid_user_input(appid, name)    
 
 def get_tuples_from_games(gamesdict: dict):
     gamelist = list(gamesdict.get('applist').get('apps'))
@@ -31,31 +31,37 @@ def get_tuples_from_games(gamesdict: dict):
     print('There are' , len(sorted_tuple_list), 'applications/games in the database.')
     return sorted_tuple_list
 
-urldb = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/'
-cwpath = getcwd() + '/'
-filename = cwpath + 'appiddump/'
-db_filename = filename + 'Steam_game_db'
+if __name__ == '__main__':
+    main()
 
-if isfile(db_filename):                                             # check if we can load the local Steam app database                      
-    print("the local database " + db_filename + " exists!")
+def main():
+    urldb = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/'
+    cwpath = getcwd() + '/'
+    filename = cwpath + 'appiddump/'
+    db_filename = filename + 'Steam_game_db'
 
-    # load a local copy of the Trie database
-    f = open(db_filename, 'rb')
-    root_trie = pickle.load(f)                                      
-    f.close()
-else:
-    print("the local database " + db_filename + " DOESNT EXIST!")
+    if isfile(db_filename):                                             # check if we can load the local Steam app database                      
+        print("the local database " + db_filename + " exists!")
 
-    json_dict = get_gameslist(urldb, filename)                      # send a GET request to Steam and get its response
-    sorted_games_tuple_list = get_tuples_from_games(json_dict)      # convert the dict to a list of tuples and sort by name alphabetically
-    root_trie = trie.build_trie_from_games(sorted_games_tuple_list) # insert each tuple to make a Trie (gives us searchable prefixes)
+        # load a local copy of the Trie database
+        f = open(db_filename, 'rb')
+        root_trie = pickle.load(f)                                      
+        f.close()
+    else:
+        print("the local database " + db_filename + " DOESNT EXIST!")
 
-    # store (dump) the new Trie database locally
-    f = open(db_filename, 'wb')
-    setrecursionlimit(3000)
-    pickle.dump(root_trie, f)
-    f.close()
+        json_dict = get_gameslist(urldb, filename)                      # send a GET request to Steam and get its response
+        sorted_games_tuple_list = get_tuples_from_games(json_dict)      # convert the dict to a list of tuples and sort by name alphabetically
+        root_trie = trie.build_trie_from_games(sorted_games_tuple_list) # insert each tuple to make a Trie (gives us searchable prefixes)
 
-# search the Trie by application title, and return its appid.
-search_results = trie.search_trie(root_trie, 'Squad')
-print(search_results)
+        # store (dump) the new Trie database locally
+        f = open(db_filename, 'wb')
+        setrecursionlimit(3000)
+        pickle.dump(root_trie, f)
+        f.close()
+
+    return root_trie
+    # search the Trie by application title, and return its appid.
+
+    # search_results = trie.search_trie(root_trie, title)
+    # print(search_results)
--- guidbinput.py
+++ guidbinput.py
@@ -2,44 +2,60 @@
 
 
 import PySimpleGUI as sg
+from trie import search_trie
+from appiddb import main
 
 sg.theme('DarkAmber')
 
-statustext = sg.Text(key='-OUTPUT-', text='Waiting on you below...', text_color='white', size=(50, 2))
+statustext = sg.Text(key='-OUTPUT-', text='Enter Steam game/app..', text_color='white', size=(50, 2))
+outputtext = sg.Text('', size=(50, 2), visible=False, key='-OUTPUT2-')
 
 layout =    \
     [
-        [sg.Text('Status:'), statustext],
+        [sg.Text('Status:', size=(8, 2)), statustext],
         [sg.Input(key='-IN-')],
-        [sg.Button('Show', auto_size_button=True), sg.Button('Exit', auto_size_button=True)]
+        [sg.Button('Show', auto_size_button=True), sg.Button('Exit', auto_size_button=True)],
+        [outputtext]
     ]
 
 window = sg.Window('Steam appid sample GUI', layout, size=(1600, 800), auto_size_text=True, auto_size_buttons=True)
 
+root_trie = main()
+
 while True:  # Event Loop
     event, values = window.read()
-    # print(event, values)
+
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     if event == 'Show':
-        # Update the "output" text element to be the value of "input" element
-        # window['-OUTPUT-'].update(values['-IN-'])
         game_title = str(values['-IN-'])
 
         if game_title in ('', len(game_title) <= 0):
-
-            status_row = layout[0]
-            status_text_ind = status_row[1]
-            status_raw = 'Error - you entered nothing. \n Please type the game/application in the box below.'
-  
-
-
+            status_raw = 'Error - you entered nothing.\nPlease type the game/application in the box below.'
             window['-OUTPUT-'].update(statustext.update(value=status_raw, text_color='red'))
-        
+        else:
+            status_raw = 'Searching...'
+            window['-OUTPUT-'].update(statustext.update(value=status_raw, text_color='yellow'))
 
+            search_results = search_trie(root_trie, game_title)
+
+            if len(search_results) <= 0:
+                status_raw = 'Error - no results. \n Try typing the name of a different game/application.'
+                window['-OUTPUT-'].update(statustext.update(value=status_raw, text_color='red'))
+            else:
+                status_raw = 'Ready!'
+                window['-OUTPUT-'].update(statustext.update(value=status_raw, text_color='green'))
+
+                first_result = search_results[0]
+                title_raw = 'This was the first result: ' + first_result + '\nIs this correct?'
+                window['-OUTPUT2-'].update(outputtext.update(value=title_raw, text_color='cyan', visible=True))
 
 window.close()
 
+
+
+# Update the "output" text element to be the value of "input" element
+# window['-OUTPUT-'].update(values['-IN-'])
 
 
 
